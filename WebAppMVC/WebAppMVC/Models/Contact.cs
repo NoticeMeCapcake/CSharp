e080723bb4bab09c8af8725087fc23124cc23e70
NoticeMeCapcake <danko3033@gmail.com>
2023-03-18 01:24:07 +0300

    some improvement

---
+++ WebAppMVC/WebAppMVC/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppMVC.Models;
+
+public class Contact
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int age { get; set; }
+    public string Email { get; set; }
+    public string message { get; set; }
+}
