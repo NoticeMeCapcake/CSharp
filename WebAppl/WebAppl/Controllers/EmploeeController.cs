9578e83ff6d79343f9217bda43713cb09bc0ffa4
NoticeMeCapcake <danko3033@gmail.com>
2023-03-20 00:03:32 +0300

    Added employee controller

--- WebAppl/WebAppl/Controllers/EmploeeController.cs
+++ WebAppl/WebAppl/Controllers/EmploeeController.cs
@@ -1,52 +1,100 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
 using WebAppl.DbRepo;
 
-namespace WebAppl.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmploeeController : ControllerBase
-    {
-        private readonly WebApiContext dbContext;
-        public EmploeeController(WebApiContext dbContext) {
-            this.dbContext = dbContext;
-        }
+namespace WebAppl.Controllers;
 
-        // GET: api/Emploee
-        [HttpGet]
-        public IEnumerable<Employee> Get()
-        {
-            return dbContext.Employees.ToList();
-        }
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : ControllerBase {
+    private readonly IConfiguration _configuration;
 
-        // GET: api/Emploee/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    public EmployeeController(IConfiguration configuration) {
+        _configuration = configuration;
+        
+    }
 
-        // POST: api/Emploee
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
+    [HttpGet]
+    public JsonResult Get() {
+        string query = "SELECT Employee.EmployeeId, Employee.EmployeeName, Department.DepartmentName, convert(varchar(10), DateOfJoining, 120) as DateOfJoining " +
+                       "FROM Employee, Department WHERE Employee.DepartmentId = Department.DepartmentId";
+        DataTable sqlDataTable = new DataTable();
+        MySqlDataReader reader;
+        using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"))) {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                reader = cmd.ExecuteReader();
+                sqlDataTable.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
         }
-
-        // PUT: api/Emploee/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
+        Console.WriteLine("Hello World");
+        Console.WriteLine(new JsonResult(sqlDataTable));
+        return new JsonResult(sqlDataTable);
+    } 
+    [HttpPost]
+    public JsonResult Post(Employee emp) {
+        string query = @"INSERT INTO Employee (EmployeeName, DepartmentId, Age, Photo) 
+                    VALUES (@EmployeeName, @DepartmentId, @Age, @Photo)";
+        DataTable sqlDataTable = new DataTable();
+        using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"))) {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                cmd.Parameters.AddWithValue("@EmployeeName", emp.EmployeeName);
+                cmd.Parameters.AddWithValue("@EmployeeId", (Object)emp.EmployeeId);
+                cmd.Parameters.AddWithValue("@DepartmentId", emp.DepartmentId == null ? DBNull.Value : (Object)emp.DepartmentId);
+                cmd.Parameters.AddWithValue("@Photo", emp.Photo);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                sqlDataTable.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
         }
-
-        // DELETE: api/Emploee/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        Console.WriteLine("Hello World");
+        Console.WriteLine(new JsonResult(sqlDataTable));
+        return new JsonResult("Insertion success");
+    }
+    [HttpPut]
+    public JsonResult Put(Employee emp) {
+        string query = @"UPDATE Employee SET EmployeeName = @DepartmentName, DepartmentId = @DepartmentId, Age = @Age, Photo = @Photo 
+                WHERE EmployeeId = @EmployeeId";
+        DataTable sqlDataTable = new DataTable();
+        using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"))) {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                cmd.Parameters.AddWithValue("@EmployeeName", emp.EmployeeName);
+                cmd.Parameters.AddWithValue("@EmployeeId", (Object)emp.EmployeeId);
+                cmd.Parameters.AddWithValue("@DepartmentId", (Object)emp.DepartmentId);
+                cmd.Parameters.AddWithValue("@Age", (Object)emp.Age);
+                cmd.Parameters.AddWithValue("@Photo", emp.Photo);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                sqlDataTable.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
         }
+        Console.WriteLine("Hello World");
+        Console.WriteLine(new JsonResult(sqlDataTable));
+        return new JsonResult("Update success");
+    }
+    [HttpDelete("{id}")]
+    public JsonResult Delete(int id) {
+        string query = @"DELETE FROM Employee WHERE EmployeeId = @EmployeeId";
+        DataTable sqlDataTable = new DataTable();
+        using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"))) {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                cmd.Parameters.AddWithValue("@EmployeeId", (Object) id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                sqlDataTable.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+        }
+        Console.WriteLine("Hello World");
+        Console.WriteLine(new JsonResult(sqlDataTable));
+        return new JsonResult("Delete success");
     }
 }
