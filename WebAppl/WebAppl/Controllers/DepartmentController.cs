d294ad56672caabc5e98a78046a4232308195aad
NoticeMeCapcake <danko3033@gmail.com>
2023-03-19 20:15:05 +0300

    department controller is ready

--- WebAppl/WebAppl/Controllers/DepartmentController.cs
+++ WebAppl/WebAppl/Controllers/DepartmentController.cs
@@ -1,8 +1,7 @@
 ﻿using System.Data;
-using System.Data.Common;
 using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
 using MySqlConnector;
+using WebAppl.DbRepo;
 
 namespace WebAppl.Controllers;
 
@@ -29,13 +28,70 @@ public class DepartmentController : ControllerBase {
                 sqlDataTable.Load(reader);
                 reader.Close();
                 conn.Close();
-
             }
         }
-        
         Console.WriteLine("Hello World");
         Console.WriteLine(new JsonResult(sqlDataTable));
-
         return new JsonResult(sqlDataTable);
+    } 
+    [HttpPost]
+    public JsonResult Post(Department dep) {
+        string query = @"INSERT INTO Department (DepartmentName) VALUES (@DepartmentName)";
+        DataTable sqlDataTable = new DataTable();
+        Console.WriteLine(_configuration.GetConnectionString("DefaultConnection"));
+        using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"))) {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                cmd.Parameters.AddWithValue("@DepartmentName", dep.DepartmentName);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                sqlDataTable.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+        }
+        Console.WriteLine("Hello World");
+        Console.WriteLine(new JsonResult(sqlDataTable));
+        return new JsonResult("Insertion success");
+    }
+    [HttpPut]
+    public JsonResult Put(Department dep) {
+        string query = @"UPDATE Department SET DepartmentName = @DepartmentName 
+                  WHERE DepartmentId = @DepartmentId";
+        DataTable sqlDataTable = new DataTable();
+        Console.WriteLine(_configuration.GetConnectionString("DefaultConnection"));
+        using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"))) {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                cmd.Parameters.AddWithValue("@DepartmentId", (Object)dep.DepartmentId);
+                cmd.Parameters.AddWithValue("@DepartmentName", dep.DepartmentName);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                sqlDataTable.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+        }
+        Console.WriteLine("Hello World");
+        Console.WriteLine(new JsonResult(sqlDataTable));
+        return new JsonResult("Update success");
+    }
+    [HttpDelete("{id}")]
+    public JsonResult Delete(int id) {
+        string query = @"DELETE FROM Department 
+                  WHERE DepartmentId = @DepartmentId";
+        DataTable sqlDataTable = new DataTable();
+        Console.WriteLine(_configuration.GetConnectionString("DefaultConnection"));
+        using (MySqlConnection conn = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"))) {
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                cmd.Parameters.AddWithValue("@DepartmentId", (Object) id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                sqlDataTable.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+        }
+        Console.WriteLine("Hello World");
+        Console.WriteLine(new JsonResult(sqlDataTable));
+        return new JsonResult("Delete success");
     }
 }
